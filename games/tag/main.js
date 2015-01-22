fca1bf75ceb604bbea3d06f89e177e4ae74be454
Steven! Ragnarök <steven@nuclearsandwich.com>
2015-01-22 00:24:30 -0800

    Bears do run.

--- games/tag/main.js
+++ games/tag/main.js
@@ -1,37 +1,62 @@
 // Initialize enchant.js
 enchant();
 
+// Helper functions
+var helpers = {
+  randomx : function(width) {
+    return Math.floor(Math.random() * width);
+  },
+  randomy : function(height) {
+    return Math.floor(Math.random() * height);
+  },
+  randomdirection : function() {
+    // like a dice roll. 1 - 3 go right, 4 - 6 go left likewise up and down.k
+    var x = Math.floor(Math.random() * 6) - 3;
+    var y = Math.floor(Math.random() * 6) - 3;
+    return { x : x, y : y };
+  },
+  nearest : function(from, others) {
+    var distance = function(x1, y1, x2, y2) {
+      return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+    return others.sort(function(other){
+      return distance(from.x, from.y, other.x, other.y);
+    })[0];
+  }
+}
+
 // window.onload executes when the static page is finished loading.
 window.onload = function(){
     var game = new Core(400, 320);
     game.fps = 15;
     game.preload("chara1.png");
+    game.scale = 1.5;
     var characters = {
       player: 10,
       runner: 0,
       it: 5
     }
+
     game.onload = function(){
-        var bear = new Sprite(32, 32);
-
-        bear.image = game.assets["chara1.png"];
-        bear.x = game.width / 2;
-        bear.y = game.height / 2;
-        bear.character = characters.it;
-        bear.frame = bear.character;
-        game.rootScene.addChild(bear);
-
-        bear.addEventListener("enterframe", function(){
+        var player = new Sprite(32, 32);
+        player.image = game.assets["chara1.png"];
+        player.x = game.width / 2;
+        player.y = game.height / 2;
+        player.character = characters.player;
+        player.frame = player.character;
+        game.rootScene.addChild(player);
+        player.addEventListener("enterframe", function(){
           var speed = 3;
             if (game.input.up && this.y - speed >= 0) {
               this.y -= speed;
               this.frame = this.age % 2 + this.character;
             }
-            if (game.input.down && this.y + speed <= game.height - bear.height) {
+            if (game.input.down && this.y + speed <= game.height - player.height) {
               this.y += speed;
               this.frame = this.age % 2 + this.character;
             }
-            if (game.input.right && this.x + speed <= game.width - bear.width) {
+            if (game.input.right && this.x + speed <= game.width - player.width) {
               this.x += speed;
               this.frame = this.age % 2 + this.character;
             }
@@ -41,9 +66,62 @@ window.onload = function(){
             }
         });
 
-        bear.addEventListener("touchstart", function(){
-            game.rootScene.removeChild(bear);
+        var runners = [];
+        for (var r = 0; r < 5; r++) {
+          runners[r] = new Sprite(32, 32);
+          runners[r].image = game.assets["chara1.png"];
+          runners[r].x = helpers.randomx(game.width - runners[r].width);
+          runners[r].y = helpers.randomy(game.height - runners[r].height);
+          runners[r].character = characters.runner;
+          runners[r].frame = runners[r].character;
+          runners[r].direction = helpers.randomdirection();
+          runners[r].addEventListener("enterframe", function(){
+            nextX = this.x + this.direction.x;
+            if (nextX > 0 && nextX <= game.width - this.width) {
+              this.x += this.direction.x;
+            } else {
+              this.direction.x *= -1;
+            }
+
+            nextY = this.y + this.direction.y;
+            if (nextY > 0 && nextY <= game.height - this.height) {
+              this.y += this.direction.y;
+            } else {
+              this.direction.y *= -1;
+            }
+
+            this.frame = this.age % 2 + this.character;
+          });
+
+          game.rootScene.addChild(runners[r]);
+        }
+
+        var it = new Sprite(32, 32);
+        it.image = game.assets["chara1.png"];
+        it.x = helpers.randomx(game.width - it.width);
+        it.y = helpers.randomy(game.height - it.height);
+        it.character = characters.it;
+        it.frame = it.character;
+        helpers.nearest(this, runners)
+        it.addEventListener("enterframe", function(){
+          var nearest = helpers.nearest(this, runners)
+          if (nearest.x > this.x) {
+            this.x += 3
+          } else if (nearest.x < this.x) {
+            this.x -= 3;
+          }
+
+          if (nearest.y > this.y) {
+            this.y += 3
+          } else if (nearest.y < this.y) {
+            this.y -= 3;
+          }
+          this.frame = this.age % 2 + this.character;
+
         });
+        game.rootScene.addChild(it);
+
+        console.log("Game is starting!");
     };
     game.start();
 };
